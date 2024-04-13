af7e4fb15ddef5ded8d0abee7e94780d51e7dadb
augustojosemagalhaesmattos <augustommjf@gmail.com>
2024-04-12 21:25:47 -0300

    primeiro hello world

---
+++ aula001_primeiro_programa/aula01.py
@@ -0,0 +1,17 @@
+# autor: Augusto Mattos
+# Data: 12/04/2024
+# Primeiro Programa em Python
+
+# Importando as bibliotecas
+import os
+
+
+# Limpando o Terminal
+os.system('cls')
+
+# O comando print() executa uma saida via terminal
+
+print('-' * 70)
+print('Hello World!!!')
+print('-' * 70)
+print()
