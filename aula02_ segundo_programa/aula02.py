73e660c4b10f811c3df780daaa364d6614699b62
augustojosemagalhaesmattos <augustommjf@gmail.com>
2024-04-16 21:39:17 -0300

    f string

--- aula02_ segundo_programa/aula02.py
+++ aula02_ segundo_programa/aula02.py
@@ -21,6 +21,12 @@ PI = 3.14 # Isso é uma CONSTANTE, seu valor não deve ser alterado
 # Saida utulizando o método type() para verificar o
 # tipo da variável
 print('\033[0M \033[31mTipos declarados:\033[0m')
-print('\033[0m A var \033[0mé do tipo: ', type(nome))
-print('\033[0m A var \033[32m nascimento \033[0m é do tipo: ', type(nascimneto))
-print('s\033[0m A var \033[32m nascimento \033[0m é do tipo: ', type(nascimento))
+print('\033[0m A var \033[32m nome \033[0m é do tipo:', type(nome))
+print('\033[0m A var \033[32m nascimneto \033[0m é do tipo:', type(nascimento))
+print('\033[0m A var \033[32m altura \033[0m é do tipo:', type(altura))
+print('\033[0m A var \033[32m peso \033[0m é do tipo:', type(peso))
+print('\033[0m A var \033[32m doador \033[om é do tipo:', type(doador))
+print('\033[0m A var \033[32m complexo \033[0m é do tipo:', type(complexo))
+print('\033[0m A var \033[32m PI \033[0m é do tipo: ', type(PI))
+print('-'*70)
+('')
